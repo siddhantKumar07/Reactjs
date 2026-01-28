23a7dd7a3972980f463a9b0895eb21ae4201fac1
siddhantKumar07 <siddhantk80044@gmail.com>
2026-01-28 13:40:29 +0530

    updating the name

---
+++ firstday/app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {id:"head"   },"hello this is created by me using react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
