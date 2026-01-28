a492504c279844f713c8a927e8bf6410c43bf872
siddhantKumar07 <siddhantk80044@gmail.com>
2026-01-28 13:04:35 +0530

    updated project files

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int deppak[5];
+    printf("enter the numbers");
+    for(int i=0;i<5;i++)
+    {
+        scanf("%d",&deppak[i]);
+    }   
+    
+return 0;
+}
