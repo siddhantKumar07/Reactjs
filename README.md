ba619d567c08a5d3baf253056a82f67d9bb5edb7
siddhantKumar07 <siddhantk80044@gmail.com>
2026-02-08 13:15:56 +0530

    day2

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # React JS learning
 
+<<<<<<< HEAD
 This is a React.js .
 
 ## Getting Started
 
 Add your project description and setup instructions here.
+=======
+i am learning react js from scratch..............
+
+## Getting Started
+>>>>>>> 8274194 (initial commit)
--- firstday/app.js
+++ firstday/app.js
@@ -1,3 +1,24 @@
-const heading = React.createElement("h1", {id:"head"   },"hello this is created by me using react");
+// const heading = React.createElement("h1", {id:"head"   },"hello this is created by me using react");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "h1",
+    { id: "head" },
+    "hello this is created by me using react",
+  ),
+  [
+    React.createElement(
+      "div",
+      { id: "child1" },
+      React.createElement("h1", {}, "i am present in child1 div"),
+    ),
+    React.createElement(
+      "div",
+      { id: "child2" },
+      React.createElement("h1", {}, "i am present in child2 div"),
+    ),
+  ],
+]);
+
+root.render(parent);
