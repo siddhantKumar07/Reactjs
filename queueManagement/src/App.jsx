087be01e9485b0ab489eba930da16f6c7bf405e2
siddhantKumar07 <siddhantk80044@gmail.com>
2026-02-12 21:23:47 +0530

    creating a queue management system

---
+++ queueManagement/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+export default function App() {
+  return (
+    <div className="app">
+     <header>
+       <h1>Queue Management System</h1>
+        <p>Manage your customer here</p>
+     </header>
+    </div>
+  );
+}
