0263251a7a85b80c79c680283a978a46650035fb
siddhantKumar07 <siddhantk80044@gmail.com>
2026-02-15 00:03:52 +0530

    learning components building

---
+++ 01Componentss/src/App.jsx
@@ -0,0 +1,25 @@
+
+ import React from 'react'
+ import Card from './components/card.jsx'
+ function App() {
+   return (
+    < >
+ <Card />
+ <Card />
+ <Card />
+ <Card />
+ <Card />
+ <Card />
+ <Card />
+ <Card />
+ <Card />
+ <Card />
+ <Card />
+ <Card />
+
+    
+    </>  
+   )
+ }
+ 
+ export default App
---
+++ 01Componentss/src/components/card.jsx
@@ -0,0 +1,11 @@
+export default function Card() {
+  return (
+    <>
+      <div className="card">
+        <h1>Card Title</h1>
+        <p>This is a simple card component.</p>
+        <button>Click Me</button>
+      </div>
+    </>
+  );
+}
