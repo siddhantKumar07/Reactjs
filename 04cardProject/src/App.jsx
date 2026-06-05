7ebd8c08960f76dae85da1cb3e9efbe2a16b7b8d
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-05 23:17:48 +0530

    creating a card

--- 04cardProject/src/App.jsx
+++ 04cardProject/src/App.jsx
@@ -7,28 +7,28 @@ const App = () => {
       img="https://images.unsplash.com/photo-1578632292335-df3abbb0d586?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bW9kZWxpbmd8ZW58MHx8MHx8fDA%3D"
       name="siddhant"
       position="software developer"
-      money ="$250"
+      money ="$250/hr"
       address="gorakhpur, up"
       />
       <Card 
       img="https://i.pinimg.com/736x/a2/a6/f9/a2a6f95f90f031518b623bd7838e735f.jpg"
       name="mahendra"
       position="cricketer"
-      money ="$850"
+      money ="$850/hr"
       address="ranchi, jk"
       />
       <Card 
       img="https://i.pinimg.com/736x/52/a1/de/52a1de8e7beec0fd18f615ed1fb2cd9f.jpg"
       name="someone"
       position="software engineer"
-      money ="$150"
+      money ="$150/hr"
       address="somethingpur"
       />
       <Card 
       img="https://i.pinimg.com/736x/fd/b3/ed/fdb3ed2fe8a5f9ce092796cd987d462c.jpg"
       name="another singh"
       position="web developer"
-      money ="$50"
+      money ="$50/hr"
       address="america, usa"
       />
     </div>
