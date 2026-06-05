d0815afe7229739abf46465cadada63b034471ac
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-05 19:54:37 +0530

    react

--- 04cardProject/src/components/card.jsx
+++ 04cardProject/src/components/card.jsx
@@ -9,7 +9,28 @@ const card = () => {
         Save
       </button>
      </div>
-     
+     <div className="mid">
+      <h3 className='name'>Name</h3>
+      <p className='position'>POsition</p>
+      <div className="btns">
+        <button>Part time</button>
+        <button>senior level</button>
+      </div>
+     </div>
+     <footer>
+      <div className="left">
+        <div className="money">
+          <h3>250$</h3>
+        </div>
+        <div className="address">
+          <h3>Mumbai</h3>
+        </div>
+      </div>
+<div className="right">
+  <button>Apply now</button>
+</div>
+     </footer>
+
     </div>
   )
 }
