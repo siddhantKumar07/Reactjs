adcaf47c9e0e09f496477c62a715503c056dc2c6
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-07 21:20:28 +0530

    creating project through react and tailwind

---
+++ 08uiProject/src/components/section1/rightimg.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const rightimg = () => {
+  return (
+       <div className="w-2/6 h-full bg-green-400 rounded-3xl bg-[url('https://i.pinimg.com/736x/c7/48/fb/c748fb01d4395f41f17750a1ccec5749.jpg')] bg-cover bg-center bg-no-repeat flex flex-col justify-between pb-6">
+        <div className="h-9 w-9 rounded-full text-black bg-white mx-2 my-5 text-center">
+          3
+        </div>
+         <p className=" w-[75%] mt-8 mx-4 ">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+          nemo eos illo earum obcaecati
+        </p>
+        <div className=" h-20 flex justify-between w-[80%] items-center ml-4"> <div className="bg-green-400 h-12 w-[70%] rounded-4xl text-center pt-2"> satisfied</div>
+        <div className="h-8 w-12 bg-green-400 overflow-hidden rounded-t-lg rounded-b-lg"></div>
+            <div className="h-12 w-14 text-2xl text-black rounded-full bg-green-400 text-center pt-2"><i className="ri-arrow-right-line "></i></div>
+
+        </div>
+      </div>
+  )
+}
+
+export default rightimg
