1a02e96335faefc0867c1560c9a887dd73b91dcd
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-07 20:56:27 +0530

    creating project through react and tailwind

--- 08uiProject/src/components/section1/rightContent.jsx
+++ 08uiProject/src/components/section1/rightContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const rightContent = () => {
   return (
-    <div className="bg-gray-700 h-[calc(100vh-8rem)] w-[75%] p-5 flex gap-8">
+    <div className=" h-[calc(100vh-8rem)] w-[75%] p-5 flex gap-8">
       <div className="w-2/6 rounded-3xl bg-[url('https://i.pinimg.com/736x/49/35/6a/49356a364e280e2ede66e661905e43b0.jpg')] bg-cover bg-center bg-no-repeat flex flex-col justify-between pb-6">
         <div className="h-9 w-9 rounded-full text-black bg-white mx-2 my-5 text-center">
           1
@@ -34,7 +34,7 @@ const rightContent = () => {
         </p>
         <div className=" h-20 flex justify-between w-[80%] items-center"> <div className="bg-green-400 h-18 w-[70%] rounded-4xl text-center pt-4"> satisfied</div>
         <div className="h-8 w-12 bg-green-400 overflow-hidden rounded-t-lg rounded-b-lg"></div>
-            <div className="h-12 w-12 rounded-full bg-green-400"></div>
+            <div className="h-12 w-14 text-2xl text-black rounded-full bg-green-400 text-center pt-2"><i className="ri-arrow-right-line "></i></div>
 
         </div>
       </div>
