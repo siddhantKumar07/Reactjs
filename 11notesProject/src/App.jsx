8d5ba778e6c9bce2b4a44684692b45deadf0b07e
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-08 22:47:17 +0530

    making notes project

--- 11notesProject/src/App.jsx
+++ 11notesProject/src/App.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 
 const App = () => {
+  const handleform=(e)=>{
+  e.preventDefault()
+  }
   return (
-    <div className='h-screen w-full bg-sky-300 p-0.5  '>
-     <form className='flex  gap-4 justify-between items-start p-7 '>
-       <div className='flex flex-col gap-8 items-start  m-15 h-[50%] w-[40%]'>
+    <div className='h-screen w-full flex bg-black p-0.5  '>
+     <form onSubmit={handleform} className='flex flex-col gap-4 justify-between  m-15 h-[50%] w-[40%] items-start p-7 bg-yellow-200'>
+
         <input type="text" 
       placeholder='Enter notes Heading'
-      className='px-4 py-3 w-full rounded text-white text-3xl bg-black'
+      className=' outline-none px-4 py-3 w-full rounded text-white  border-2 text-3xl bg-black'
       />
       
-      <input type="text"
+      <textarea type="text"
       placeholder='Enter the details'
-    className='px-4 py-8 rounded w-full text-white text-3xl bg-black'
+    className='px-4 py-8 max-h-40 outline-none rounded w-full text-white border-2 text-3xl bg-black'
       />
-      <button className='px-4 py-5 w-full bg-white text-black rounded-2xl font-bold text-2xl '>Add note</button>
-       </div>
-            <img src="https://tse2.mm.bing.net/th/id/OIP.Qn2__HNWh2ZkOmTiqORy3gHaHa?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" alt="" />
-
+      <button className='px-4 cursor-pointer py-5 w-full bg-white outline-none text-black rounded-2xl font-bold text-2xl '>Add note</button>
      </form>
+     <div className='flex bg-sky-300 h-52 w-40'></div>
     </div>
   )
 }
