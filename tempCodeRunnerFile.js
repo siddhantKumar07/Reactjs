6c3a0560f09014d72b7ffbcdece1d3546591274a
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-08 11:29:33 +0530

    learn

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-let readline = require('readline')
-// let r1 = readline.Interface({
-//     input: process.stdin,
-//     output:process.stdout
-// })
-// r1.question("write the value ",(value)=>{
-//     console.log(value)
-//     r1.close()
-// })
