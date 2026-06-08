9591ff53353f65ff5c9e4f346220d1a3582b6e27
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-08 17:36:14 +0530

    learning the form handling

---
+++ 10Formhandling/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  
+  return (
+    <div>
+      <form >
+        <input type="text" placeholder='enter your name ' />
+        <button>click</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
