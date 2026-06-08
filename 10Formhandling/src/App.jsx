d502fab13e815657c65e8cdca8866e8a32f8cdf2
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-08 17:46:15 +0530

    learnign the form handling

--- 10Formhandling/src/App.jsx
+++ 10Formhandling/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const App = () => {
-  
+  const formSubmit=(e)=>{
+    e.preventDefault();
+    console.log("form submitted")
+    console.log(e.target[0].value)
+    e.target[0].innerText=" "
+  }
   return (
     <div>
-      <form >
+      <form onSubmit={formSubmit} >
         <input type="text" placeholder='enter your name ' />
         <button>click</button>
       </form>
