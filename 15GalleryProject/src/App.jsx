5819daa0061ec99f7e3718e56390adf46e8dd438
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-10 00:08:12 +0530

    building a react project

--- 15GalleryProject/src/App.jsx
+++ 15GalleryProject/src/App.jsx
@@ -6,22 +6,30 @@ const App = () => {
 
   let printuserData ="no data available "
   if(user.length>0){
-    printuserData = user.map(()=>{
-      return "hello "
+    printuserData = user.map((elem)=>{
+      return <a href={elem.url} target='_blank'>
+        <div>
+       <div  className='h-60 w-60 rounded-3xl '>
+        <img className='h-full w-full rounded-3xl bg-cover' src={elem.download_url} alt="" />
+      </div>
+      <h3 className='text-lg text-white'>{elem.author}</h3>
+      </div>
+      </a>
+      
     })
   }
   async function getImge(){
-    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=15")
+    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=20")
     setUser(response.data)
     console.log(await user);
     
   }
   return (
-    <div>
-        <button onClick={getImge} className='h-10 text-2xl active:scale-90 cursor-pointer rounded-3xl m-5 w-20 bg-sky-500'>
+    <div className='bg-black'>
+        <button onClick={getImge} className='h-10 text-2xl active:scale-90 cursor-pointer rounded-3xl m-2 w-20 bg-sky-500'>
           click
         </button>
-        <div className='text-2xl font-bold'>
+        <div className='h-full w-full text-2xl flex gap-5 items-center  flex-wrap justify-center font-bold'>
           {printuserData}
         </div>
     </div>
