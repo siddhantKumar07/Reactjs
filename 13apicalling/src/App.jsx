65c5a2e264f0364fc4962318277eca688e7a3cc8
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-09 15:02:21 +0530

    learned axios

---
+++ 13apicalling/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios from 'axios'
+const App = () => {
+  const apis =async()=>{
+    // const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    // const data = await response.json();
+    // console.log(data)
+    const response = await axios('https://jsonplaceholder.typicode.com/users')
+    console.log(response.data)
+  }
+  return (
+    <div><button onClick={apis}> click</button></div>
+  )
+}
+
+export default App
