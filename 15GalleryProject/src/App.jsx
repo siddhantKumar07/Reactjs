5bfd3851358921040ef34c316c00437213371d12
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-09 20:12:47 +0530

    building the gallary project

--- 15GalleryProject/src/App.jsx
+++ 15GalleryProject/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import axios from 'axios'
 const App = () => {
+  async function getImge(){
+    const response = await axios.get("https://picsum.photos/v2/list")
+    console.log(response.data)
+  }
   return (
-    <div>App</div>
+    <div>
+        <button onClick={getImge} className='h-10 text-2xl active:scale-90 cursor-pointer rounded-3xl m-5 w-20 bg-sky-500'>
+          click
+        </button>
+    </div>
   )
 }
 
