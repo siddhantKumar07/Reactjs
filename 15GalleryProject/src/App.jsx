b2604923c4732a96283a8071f7659208b10c21ae
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-10 12:06:29 +0530

    adding some logics by me

--- 15GalleryProject/src/App.jsx
+++ 15GalleryProject/src/App.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 const App = () => {
   const [user, setUser] = useState([])
+  const [nextCount, setnextCount] = useState(2)
+  const [previousCount, setPreviousCount] = useState(nextCount
+    
+  )
   
 
   let printuserData ="no data available "
@@ -18,14 +22,24 @@ const App = () => {
       
     })
   }
+  // when the user click the previous button it will decrease the page number of the api
   const previousData = async ()=>{
-    const response = await axios.get("https://picsum.photos/v2/list?page=3&limit=20")
+    setPreviousCount(previousCount>1?previousCount-1:previousCount)
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${previousCount}&limit=20`)
     setUser(response.data) 
+    setnextCount(previousCount+1)
   }
+
+
+  // when user click on next this function runs and increase the page number of the api 
   const nextData = async ()=>{
-const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=20")
+    setnextCount(nextCount+1);
+const response = await axios.get(`https://picsum.photos/v2/list?page=${nextCount}&limit=20`)
     setUser(response.data) 
+    setPreviousCount(nextCount-1)
   }
+
+  // it will run once when the pg load 
    async function getData(){
     const response = await axios.get("https://picsum.photos/v2/list?page=1&limit=20")
     setUser(response.data) 
@@ -34,7 +48,10 @@ const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=20"
   // this will called once when the page open or loads 
   useEffect(() => {
   getData()
-  },[user])
+  },[])
+  // useEffect(() => {
+
+  // },[user])
   
  
   return (
@@ -43,8 +60,8 @@ const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=20"
           {printuserData}
         </div>
         <div className='flex items-center justify-center py-6 bg-amber-300 gap-4.5'>
-          <button onClick={previousData} className='bg-sky-600 rounded text-black px-6 py-3 font-bold '>Prev</button>
-          <button onClick={nextData} className='bg-sky-600 rounded text-black px-6 py-3 font-bold '>Next</button>
+          <button onClick={previousData} className='bg-sky-600 cursor-pointer active:scale-95 rounded text-black px-6 py-3 font-bold '>Prev</button>
+          <button onClick={nextData} className='bg-sky-600 rounded cursor-pointer active:scale-95 text-black px-6 py-3 font-bold '>Next</button>
         </div>
     </div>
   )
