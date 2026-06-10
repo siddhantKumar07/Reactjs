e465505bbf8f02b95eda067e6e8f3f81ff7c8050
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-10 11:30:39 +0530

    adding some logics by me

--- 15GalleryProject/src/App.jsx
+++ 15GalleryProject/src/App.jsx
@@ -18,21 +18,33 @@ const App = () => {
       
     })
   }
+  const previousData = async ()=>{
+    const response = await axios.get("https://picsum.photos/v2/list?page=3&limit=20")
+    setUser(response.data) 
+  }
+  const nextData = async ()=>{
+const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=20")
+    setUser(response.data) 
+  }
    async function getData(){
-    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=20")
+    const response = await axios.get("https://picsum.photos/v2/list?page=1&limit=20")
     setUser(response.data) 
   }
 
   // this will called once when the page open or loads 
   useEffect(() => {
   getData()
-  },[])
+  },[user])
   
  
   return (
     <div className='bg-black'>
         <div className='h-full w-full text-2xl text-sky-700 flex gap-5 items-center  flex-wrap justify-center font-bold'>
           {printuserData}
+        </div>
+        <div className='flex items-center justify-center py-6 bg-amber-300 gap-4.5'>
+          <button onClick={previousData} className='bg-sky-600 rounded text-black px-6 py-3 font-bold '>Prev</button>
+          <button onClick={nextData} className='bg-sky-600 rounded text-black px-6 py-3 font-bold '>Next</button>
         </div>
     </div>
   )
