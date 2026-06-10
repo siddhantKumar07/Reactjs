0201f643f80ec33ea61994b33fd6af92359f5f62
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-11 00:24:13 +0530

    learning context api

---
+++ 18ContextApi/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+       <h1>Siddhan kumar</h1>
+    </div>
+  )
+}
+
+export default Nav
---
+++ 18ContextApi/src/components/Nav2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav2 = () => {
+  return (
+    <div className='nav2'>
+        <h2>home</h2>
+        <h2>about</h2>
+        <h2>contact</h2>
+    </div>
+  )
+}
+
+export default Nav2
