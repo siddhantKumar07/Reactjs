abfdd592c4c10c6be67ca5c687fce7ae72c1c307
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-10 16:18:58 +0530

    going to learn browser router

--- 16reactRouterDom/src/App.jsx
+++ 16reactRouterDom/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Contact from './pages/contact'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <nav className="nav">
+        <h2>Siddhant kumar</h2>
+        <links>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+        </links>
+      </nav>
+      <Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/about' element={<About/>}/>
+  <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </div>
   )
 }
 
