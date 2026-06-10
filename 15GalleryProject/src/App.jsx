36594561fc996dbbcd1da7bdbf4f7e7b06d60627
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-10 14:40:51 +0530

    Completing the project

--- 15GalleryProject/src/App.jsx
+++ 15GalleryProject/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   // when the user click the previous button it will decrease the page number of the api
   const previousData = async ()=>{
     setPreviousCount(previousCount>1?previousCount-1:previousCount)
-    const response = await axios.get(`https://picsum.photos/v2/list?page=${previousCount}&limit=20`)
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${previousCount}&limit=10`)
     setUser(response.data) 
     setnextCount(previousCount+1)
   }
@@ -28,14 +28,14 @@ const App = () => {
   // when user click on next this function runs and increase the page number of the api 
   const nextData = async ()=>{
     setnextCount(nextCount+1);
-const response = await axios.get(`https://picsum.photos/v2/list?page=${nextCount}&limit=20`)
+const response = await axios.get(`https://picsum.photos/v2/list?page=${nextCount}&limit=10`)
     setUser(response.data) 
     setPreviousCount(nextCount-1)
   }
 
   // it will run once when the pg load 
    async function getData(){
-    const response = await axios.get("https://picsum.photos/v2/list?page=1&limit=20")
+    const response = await axios.get("https://picsum.photos/v2/list?page=1&limit=10")
     setUser(response.data) 
   }
 
@@ -51,7 +51,7 @@ const response = await axios.get(`https://picsum.photos/v2/list?page=${nextCount
           {printuserData}
         </div>
         <div className='flex items-center justify-center py-8  gap-8 '>
-          <button onClick={previousData} className='bg-sky-600 cursor-pointer active:scale-95 rounded text-black px-6 py-3 font-bold '>Prev</button>
+          <button onClick={previousData}  className='bg-sky-600 cursor-pointer active:scale-95 rounded text-black px-6 py-3 font-bold '>Prev</button>
           <h1 className='text-white font-bold'>Page no {nextCount-1}</h1>
           <button onClick={nextData} className='bg-sky-600 rounded cursor-pointer active:scale-95 text-black px-6 py-3 font-bold '>Next</button>
         </div>
