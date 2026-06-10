a21459d6d6ef5b81cb8124b9f45bd3fdc15d0324
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-10 21:11:12 +0530

    learning advance routing

--- 17Advancerouting/src/components/About.jsx
+++ 17Advancerouting/src/components/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 const About = () => {
+   let navigate = useNavigate();
   return (
    <div  className='bg-red-400 h-[89vh]'>
+      <button onClick={()=>{
+       
+           navigate('/')
+      }} className='cursor-pointer bg-sky-400 px-7 py-2.5 m-2 rounded active:scale-90'>click To home </button>
+      <button onClick={()=>{
+       
+           navigate(-1)
+      }} className='cursor-pointer bg-sky-400 px-7 py-2.5 m-2 rounded active:scale-90'>Back </button>
       <h1 className='text-9xl text-black text-center'>About page</h1>
     </div>
   )
