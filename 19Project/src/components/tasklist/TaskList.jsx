20d2a0ee5aa2e77692f54be6c029480543710f6b
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-12 23:40:03 +0530

    building the employee dashboard

--- 19Project/src/components/tasklist/TaskList.jsx
+++ 19Project/src/components/tasklist/TaskList.jsx
@@ -10,7 +10,7 @@ const TaskList = ({data}) => {
           return <div className='flex items-start gap-8 flex-col shrink-0 h-[90%] w-[25%] bg-sky-600 rounded-xl m-3 px-3.5'>
         <div key={idx} className='h-15 w-full flex items-center justify-between '>
             <h2 className='font-bold text-2xl bg-red-600 text-white rounded-xl px-2.5 py-1'>High</h2>
-            <h3>{task.taskDate}</h3>
+            <h3>{task.taskDate }</h3>
         </div>
         <h1 className='text-3xl font-bold'>{task.taskTitle}</h1>
         <p className='text-lg'>{task.taskDescription}</p>
