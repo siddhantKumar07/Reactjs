0f7841d1055c77b236865df54ea8677dde18d523
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-23 01:13:03 +0530

    making better

--- 10Formhandling/src/App.jsx
+++ 10Formhandling/src/App.jsx
@@ -1,26 +1,117 @@
 import React, { useState } from "react";
-
+import {ToastContainer,toast,Bounce} from 'react-toastify'
 const App = () => {
-  const [text, setText] = useState("");
-  const formSubmit = (e) => {
-    e.preventDefault();
-    console.log(text);
+  const [error, setError] = useState('')
+const [formData, setFormData] = useState({
+  fullName:"",
+  email:'',
+  password:'',
+  confPass:''
+})
 
-    setText(" ")
-    
-  };
+const handleChange=(e)=>{
+ const {name,value}=e.target;
+  setFormData((prev)=>({
+    ...prev,
+    [name]:value
+}))
+
+
+}
+
+const handleForm=(e)=>{
+  e.preventDefault();
+console.log(formData)
+  notify()
+
+  setError('')
+}
+
+
+
+ // toaster
+    const notify = () =>
+      toast.success("Form Submitted !!!", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
   return (
-    <div>
-      <form onSubmit={formSubmit}>
-        <input type="text" 
-        placeholder="enter your name "
-        value={text}
-        onChange={(e)=>{
-          setText(e.target.value);
+   <div className="h-screen w-full flex items-center justify-center p-1">
+      <form
+        onSubmit={(e) => {
+          handleForm(e);
         }}
+        className=" px-7 w-[30%] h-[70%] flex flex-col gap-4  justify-center bg-sky-100 rounded border-2 border-cyan-500"
+      >
+        <h1 className="text-3xl text-center font-bold underline">
+          Create an Account
+        </h1>
+        <input
+          required
+          name="fullName"
+          value={formData.fullName}
+          onChange={handleChange}
+          type="text"
+          placeholder="Enter your name"
+          className="rounded py-3 px-4 text-2xl border-2 outline-none focus:border-cyan-700 focus:border-4"
         />
-        <button className="bg-sky-400">click</button>
+        <input
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+          required
+          type="email"
+          placeholder="Enter your email"
+          className="rounded py-3 px-4 text-2xl border-2 outline-none focus:border-cyan-700 focus:border-4"
+        />
+        <input
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+          required
+          type="password"
+          placeholder="Enter your password"
+          className="rounded py-3 px-4 text-2xl border-2 outline-none focus:border-cyan-700 focus:border-4"
+        />
+        <input
+        name="confPass"
+        value={formData.confPass}
+        onChange={handleChange}
+          required
+          type="password"
+          placeholder="Confirm password"
+          className="rounded py-3 px-4 text-2xl border-2 outline-none focus:border-cyan-700 focus:border-4"
+        />
+        {error && (
+          <p className="text-lg text-red-600 font-semibold text-center">
+            {error}
+          </p>
+        )}
+        <button className="px-5 py-3 bg-emerald-800 rounded-2xl w-full active:scale-90 cursor-pointer text-xl text-white font-bold">
+          Submit
+        </button>
       </form>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="light"
+        transition={Bounce}
+      />
     </div>
   );
 };
