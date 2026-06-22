955b4212ae97c7cba673f1dbd4129e5919d35da1
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-22 12:48:51 +0530

    handling the form

---
+++ 28FormValidationForInterview/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full flex items-center justify-center p-1'>
+      <form className=' px-7 w-[30%] h-[70%] flex flex-col gap-4  justify-center bg-sky-100 rounded border-2 border-cyan-500'>
+      <h1 className='text-3xl text-center font-bold underline'>Create an Account</h1>
+        <input type="text" placeholder='Enter your name' className='rounded py-3 px-4 text-2xl border-2 outline-none focus:border-cyan-700 focus:border-4' />
+        <input type="email" placeholder='Enter your email' className='rounded py-3 px-4 text-2xl border-2 outline-none focus:border-cyan-700 focus:border-4' />
+        <input type="password" placeholder='Enter your password' className='rounded py-3 px-4 text-2xl border-2 outline-none focus:border-cyan-700 focus:border-4'/>
+        <input type="password" placeholder='Confirm password' className='rounded py-3 px-4 text-2xl border-2 outline-none focus:border-cyan-700 focus:border-4' />
+        <button className='px-5 py-3 bg-emerald-800 rounded-2xl w-full active:scale-90 cursor-pointer text-xl text-white font-bold'>Submit</button>
+      </form>
+    </div>
+
+  )
+}
+
+export default App
