8bf080a5ab5b26f24609f5789d529ba4db434d0d
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-16 13:38:58 +0530

    creating a nav

---
+++ 29SwiggyProject/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-[#FF5200] h-[95vh] w-full p-1'>
+        <div className='h-[10%] w-full px-32 border-2 mt-7'>
+          <div className='h-full w-[15%] py-2 px-2'>
+            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/static-assets/images/swiggy_logo_white.png" alt="" className='w-full h-full' />
+
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+    </header>
+  )
+}
+
+export default Header
