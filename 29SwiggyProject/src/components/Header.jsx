70434b2e4d82383e04972c5f9700742a7688f55d
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-16 21:31:42 +0530

    creating the hero section

--- 29SwiggyProject/src/components/Header.jsx
+++ 29SwiggyProject/src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { MapPin, Search } from 'lucide-react'
 const Header = () => {
   return (
-    <header className='bg-[#FF5200] h-[95vh] w-full p-1'>
-        <div className='h-[10%] w-full px-32  mt-7 flex items-center justify-between'>
+    <header className='bg-[#FF5200] h-screen w-full p-1  relative'>
+        <div className='h-[10%] w-full px-32  mt-7 flex items-center shrink-0 justify-between'>
           <div className='h-[90%] w-[15%] py-2 px-2'>
             <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/static-assets/images/swiggy_logo_white.png" alt="" className='w-full h-[90%] cursor-pointer active:scale-90' />
 
@@ -16,7 +16,23 @@ const Header = () => {
 
           </div>
         </div>
-        <div></div>
+
+        {/* left image */}
+        <img className='absolute left-0 top-[15%] h-[65%] w-[17%]' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/testing/seo-home/Veggies_new.png" alt="" />
+        <img className='absolute right-0 top-[15%] h-[65%] w-[17%]' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/testing/seo-home/Sushi_replace.png" alt="" />
+        <div className='h-[35%] w-[60%] mx-auto flex flex-col gap-10 mt-16  px-4'>
+          <h1 className='text-5xl px-10 text-center font-bold text-white'>Order food & groceries. Discover best restaurants. Swiggy it!</h1>
+          <div className=' flex items-center justify-between w-full gap-4'>
+          <div className='px-3 w-[40%]  py-4 bg-white rounded-2xl flex items-center gap-2'>
+           <MapPin size={30} color="#FF5200" />
+            <input type="text" placeholder='Enter your delivery location' className='text-lg w-full text-black font-bold placeholder:text-gray-500'  />
+          </div>
+          <div className='px-5 w-[55%] py-4 bg-white rounded-2xl flex items-center gap-2'>
+          <input type="text" placeholder='Search for restaurant, item or more' className='text-lg w-full text-black font-bold placeholder:text-gray-500' />
+          <Search size={30} color="#bababa" />
+          </div>
+          </div>
+        </div>
         <div></div>
     </header>
   )
