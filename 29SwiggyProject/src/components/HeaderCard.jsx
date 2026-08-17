9a2f2777327b60f7affe3750afa9d08f78f71763
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-17 11:28:38 +0530

    creating a card for header

--- 29SwiggyProject/src/components/HeaderCard.jsx
+++ 29SwiggyProject/src/components/HeaderCard.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { ArrowRight } from "lucide-react";
+import React from "react";
 
-const HeaderCard = () => {
+const HeaderCard = ({ first, second, third }) => {
   return (
-    <div className='h-full w-1/4 bg-white px'>
-
+    <div className="h-full w-1/4 bg-white py-2 pl-3.5 rounded-2xl">
+      <div className="flex flex-col items-start justify-start gap-1">
+        <h1 className="text-3xl uppercase text-gray-600">{first}</h1>
+        <p className="uppercase text-gray-400 text-lg">{second}</p>
+        <span className="text-[#FF5200] mt-2">{third}</span>
+      </div>
+      <div className="flex items-end justify-between">
+        <button className="rounded-full px-3 py-3 cursor-pointer"><ArrowRightt strokeWidth={2.5} /></button>
+        <img src="" alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderCard
+export default HeaderCard;
