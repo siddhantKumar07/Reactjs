6a96346d5f1cc63237e9626c00fff4eb50b6d4ea
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-19 20:06:13 +0530

    doing some changes

--- 29SwiggyProject/src/components/HeaderCard.jsx
+++ 29SwiggyProject/src/components/HeaderCard.jsx
@@ -1,11 +1,11 @@
 import { ArrowRight } from "lucide-react";
 import React from "react";
-
+import {Link} from 'react-router-dom'
 const HeaderCard = ({image }) => {
   return (
-    <div className="h-full w-2/5 bg-white  rounded-4xl cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out">
+    <Link to={'/card'} className="h-full w-2/5 bg-white  rounded-4xl cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out">
        <img src={image} alt="image" className="h-full w-full object-center scale-115 rounded-4xl" />
-    </div>
+    </Link>
   );
 };
 
