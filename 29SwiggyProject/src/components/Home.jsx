ba610b651f0aff565f5e8647947839f884c901bd
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-20 16:07:00 +0530

    added another file

--- 29SwiggyProject/src/components/Home.jsx
+++ 29SwiggyProject/src/components/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from './Header'
 import FoodOptions from './FoodOptions'
+import Groceries from './Groceries'
 const Home = () => {
   return (
   <div className='h-screen w-full flex flex-col gap-5'>
    <Header/>
    <FoodOptions/>
+   <Groceries/>
   </div>
 )
 }
