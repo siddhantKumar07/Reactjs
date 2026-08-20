9e0bce74e5b4a215fce2362a22e91a5176b0c374
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-20 15:22:12 +0530

    completed the food card

--- 29SwiggyProject/src/components/FoodCard.jsx
+++ 29SwiggyProject/src/components/FoodCard.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 const FoodCard = ({ item }) => {
   return (
-    <div  className='rounded-xl h-[50%] w-[15%]'>
-    <Link to={item.entityId} className='h-[70%] w-full'>
- <img src={`https://media-assets.swiggy.com/swiggy/image/upload/${item.imageId}`} alt="foodImage" className='h-full  w-full object-cover' />
-</Link>
+    <div className="w-full h-full rounded-xl">
+      <Link to={item.entityId} className="block w-full h-full">
+        <img
+          src={`https://media-assets.swiggy.com/swiggy/image/upload/${item.imageId}`}
+          alt="foodImage"
+          className="w-full h-full object-cover rounded-xl"
+        />
+      </Link>
     </div>
   )
 }
--- 29SwiggyProject/src/components/FoodOptions.jsx
+++ 29SwiggyProject/src/components/FoodOptions.jsx
@@ -3,11 +3,11 @@ import foodGrid from '../utils/FoodData'
 import FoodCard from './FoodCard'
 const FoodOptions = () => {
   return (
-    <div className='w-full h-full bg-sky-300 py-7 flex justify-center items-center'>
-     <div className='h-[70%] w-[85%] border-2 px-3 py-2 flex flex-nowrap gap-5 items-center overflow-scroll'>
-     {foodGrid.map((item)=>{
-        return <FoodCard key={item.id} item={item}/>
-      })}
+    <div className="w-full py-7 flex justify-center">
+      <div className="w-[85%] h-[420px] border-2 px-3 py-2 grid grid-rows-2 grid-flow-col auto-cols-[140px] gap-6 overflow-x-auto overflow-y-hidden scrollbar-none">
+        {foodGrid.map((item) => (
+          <FoodCard key={item.id} item={item} />
+        ))}
       </div>
     </div>
   )
