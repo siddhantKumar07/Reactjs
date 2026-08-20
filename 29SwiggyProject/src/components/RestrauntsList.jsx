044ddde3eaed39491e8682a04a604bc6d808fdac
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-20 21:05:04 +0530

    adding the restraunts details

--- 29SwiggyProject/src/components/RestrauntsList.jsx
+++ 29SwiggyProject/src/components/RestrauntsList.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-
+import {restaurantList} from '../utils/FoodData'
 const RestrauntsList = () => {
   return (
     <div className='h-screen w-full flex justify-center items-center'>
       <div className='w-[80%] h-full bg-emerald-300'>
   <h1 className='text-2xl font-bold text-center'>Discover best restraunts on Dineout</h1>
-    <div className='w-full h-[90%] grid  grid-rows-1 grid-flow-col gap-2 overflow-x-scroll overflow-y-hidden'>
-    {}
+    <div className='w-full h-[90%] grid  grid-rows-1 grid-flow-col  auto-cols-[250px] gap-2 overflow-x-scroll overflow-y-hidden'>
+    {restaurantList.map((restaurant)=>{
+        return(
+            <div className='h-full w-full'>
+             <img src="" alt="" />
+            </div>
+        )
+    })}
 
     </div>
       </div>
