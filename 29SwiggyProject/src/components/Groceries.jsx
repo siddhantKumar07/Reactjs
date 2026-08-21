4627247dde4cd90d55b5192d0a1ac48169ab2e4c
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-21 19:48:34 +0530

    creting the restraunts cards

--- 29SwiggyProject/src/components/Groceries.jsx
+++ 29SwiggyProject/src/components/Groceries.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import {groceriesInstamart} from '../utils/FoodData'
+import { Link } from 'react-router-dom'
 const Groceries = () => {
   return (
     <div className='h-screen w-full py-4 flex flex-col justify-center items-center'>
         <h1 className='text-2xl text-black font-bold'>Shop Groceries on Instamart</h1>
         <div className='min-h-[250px] w-[80%] grid grid-flow-col grid-rows-1 auto-cols-[250px] gap-2 overflow-x-auto overflow-y-hidden scrollbar-thin scrollbar-thumb-emerald-300 mt-10'>
          {groceriesInstamart.map((item)=>(
+           <Link to={`${item.action.link}`} className='block h-full w-full'>
+           
             <div key={item.id} className='h-[90%] px-2 py-3'>
                 <img src={`https://media-assets.swiggy.com/swiggy/image/upload/${item.imageId}`} alt="" className='w-full rounded-3xl h-[80%] object-cover' />
                 <h2 className='text-center text-xl font-bold text-gray-500'>{item.action.text}</h2>
             </div>
+            </Link>
          ))}
         </div>
     </div>
--- 29SwiggyProject/src/components/RestrauntsList.jsx
+++ 29SwiggyProject/src/components/RestrauntsList.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import {restaurantList} from '../utils/FoodData'
 const RestrauntsList = () => {
   return (
-    <div className='h-screen w-full flex justify-center items-center'>
-      <div className='w-[80%] h-full bg-emerald-300'>
+    <div className='h-screen w-full bg-emerald-300 flex justify-center items-center'>
+      <div className='w-[80%] h-full flex flex-col gap-8 '>
   <h1 className='text-2xl font-bold text-center'>Discover best restraunts on Dineout</h1>
-    <div className='w-full h-[90%] grid  grid-rows-1 grid-flow-col  auto-cols-[250px] gap-2 overflow-x-scroll overflow-y-hidden'>
+    <div className='w-full h-[90%] grid  grid-rows-1 grid-flow-col  auto-cols-[250px] gap-10 overflow-x-scroll overflow-y-hidden'>
     {restaurantList.map((restaurant)=>{
         return(
-            <div className='h-[50%] w-[250px]'>
-             <img className="h-full w-full object-cover" src={`https://media-assets.swiggy.com/swiggy/image/upload/${restaurant.mediaFiles.url}`} alt="" />
+            <div className='h-[50%] w-[250px] rounded-2xl border-2'>
+             <img className="h-full rounded-2xl w-full object-cover" src={`https://media-assets.swiggy.com/swiggy/image/upload/${restaurant.mediaFiles[0].url}`} alt="" />
             </div>
         )
        
