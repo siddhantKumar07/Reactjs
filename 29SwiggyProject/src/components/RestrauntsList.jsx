5c78423f7a22eb1ba5690fcd51db0de8236b60a4
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-21 20:10:04 +0530

    adding some ui in restraunts

--- 29SwiggyProject/src/components/RestrauntsList.jsx
+++ 29SwiggyProject/src/components/RestrauntsList.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import {restaurantList} from '../utils/FoodData'
 const RestrauntsList = () => {
   return (
-    <div className='h-screen w-full bg-emerald-300 flex justify-center items-center'>
+    <div className='min-h-[70vh] w-full bg-emerald-300 flex justify-center items-center'>
       <div className='w-[80%] h-full flex flex-col gap-8 '>
   <h1 className='text-2xl font-bold text-center'>Discover best restraunts on Dineout</h1>
-    <div className='w-full h-[90%] grid  grid-rows-1 grid-flow-col  auto-cols-[250px] gap-10 overflow-x-scroll overflow-y-hidden'>
+    <div className='w-full h-[90%] grid  grid-rows-1 grid-flow-col  auto-cols-[300px] gap-10 overflow-x-scroll overflow-y-hidden'>
     {restaurantList.map((restaurant)=>{
         return(
-            <div className='h-[50%] w-[250px] rounded-2xl border-2'>
-             <img className="h-full rounded-2xl w-full object-cover" src={`https://media-assets.swiggy.com/swiggy/image/upload/${restaurant.mediaFiles[0].url}`} alt="" />
+            <div className='h-full w-[300px] rounded-2xl border-2 relative'> 
+            <div style={{backgroundImage:`url(https://media-assets.swiggy.com/swiggy/image/upload/${restaurant.mediaFiles[0].url})`,backgroundSize: 'cover',
+    backgroundPosition: 'center',}} className='h-[50%] w-full'>
+             <h1 className='z-10'>{restaurant.name}</h1>
+            </div>
+            
+            
             </div>
         )
        
